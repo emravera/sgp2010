1546eeef6dc84e800b2b7c3bdcbc08d06e90e991
ravera.emanuel <ravera.emanuel@eeb04ab4-94a6-883e-18bf-114ae2bda777>
2010-07-25 23:37:13 +0000

    Pantalla completa de Estimar Demanda Anual- Si no les compila recuerden instalar los paquetes de graficos que les pase Ema

--- Implementacion/Sistema GyCAP/DAL/DemandaAnualDAL.cs
+++ Implementacion/Sistema GyCAP/DAL/DemandaAnualDAL.cs
@@ -32,13 +32,14 @@ namespace GyCAP.DAL
             }
             catch (SqlException) { throw new Entidades.Excepciones.BaseDeDatosException(); }
         }
+
         //Metodo para insertar
         public static int Insertar(Entidades.DemandaAnual demanda)
         {
 
             //Agregamos select identity para que devuelva el código creado, en caso de necesitarlo
             string sql = "INSERT INTO [DEMANDAS_ANUALES] ([deman_anio], [deman_fechacreacion], [deman_paramcrecimiento], [deman_nombre]) VALUES (@p0, @p1, @p2, @p3) SELECT @@Identity";
-            object[] valorParametros = { demanda.Anio, demanda.FechaCreacion, demanda.ParametroCrecimiento, demanda.Nombre };
+            object[] valorParametros = { demanda.Anio, demanda.FechaCreacion.ToShortDateString() , demanda.ParametroCrecimiento, demanda.Nombre };
             try
             {
                 return Convert.ToInt32(DB.executeScalar(sql, valorParametros, null));
@@ -47,6 +48,77 @@ namespace GyCAP.DAL
 
         }
 
+        //MODIFICAR 
+        //Metodo que modifica en la base de datos
+        public static void Actualizar(Entidades.DemandaAnual demanda)
+        {
+            string sql = @"UPDATE DEMANDAS_ANUALES SET deman_anio = @p0, deman_nombre = @p1
+                         WHERE deman_codigo = @p2";
+            object[] valorParametros = {  demanda.Anio, demanda.Nombre, demanda.Codigo };
+            try
+            {
+                DB.executeNonQuery(sql, valorParametros, null);
+            }
+            catch (SqlException) { throw new Entidades.Excepciones.BaseDeDatosException(); }
+        }
+       
+        //Metodo que valida que se pueda actualizar
+        public static bool Validar(Entidades.DemandaAnual demanda)
+        {
+            string sql = @"SELECT count(pan_codigo)
+                           FROM PLANES_ANUALES WHERE deman_codigo=@p0";
+            object[] valorParametros = { demanda.Codigo };
+            try
+            {
+                int cantidad=Convert.ToInt32(DB.executeScalar(sql, valorParametros, null));
+                if (cantidad == 0) return true;
+                else return false;
+            }
+            catch (SqlException) { throw new Entidades.Excepciones.BaseDeDatosException(); }
+        }
+        
+        //ELIMINACION
+        //Metodo que verifica que no este usado en otro lugar
+        public static bool PuedeEliminarse(int codigo)
+        {
+            string sql = "SELECT count(pan_codigo) FROM PLANES_ANUALES WHERE deman_codigo = @p0";
+            object[] valorParametros = { codigo };
+            try
+            {
+                if (Convert.ToInt32(DB.executeScalar(sql, valorParametros, null)) == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (SqlException) { throw new Entidades.Excepciones.BaseDeDatosException(); }
+        }
+
+        //Metodo que elimina de la base de datos
+        public static void Eliminar(int codigo)
+        {
+            string sql = "DELETE FROM DEMANDAS_ANUALES WHERE deman_codigo = @p0";
+            object[] valorParametros = { codigo };
+            try
+            {
+                DB.executeNonQuery(sql, valorParametros, null);
+            }
+            catch (SqlException) { throw new Entidades.Excepciones.BaseDeDatosException(); }
+        }
+        //Metodo que elimina de la base de datos el detalle
+        public static void EliminarDetalle(int codigo)
+        {
+            string sql = "DELETE FROM DETALLE_DEMANDAS_ANUALES WHERE deman_codigo = @p0";
+            object[] valorParametros = { codigo };
+            try
+            {
+                DB.executeNonQuery(sql, valorParametros, null);
+            }
+            catch (SqlException) { throw new Entidades.Excepciones.BaseDeDatosException(); }
+        }
 
 
     }
